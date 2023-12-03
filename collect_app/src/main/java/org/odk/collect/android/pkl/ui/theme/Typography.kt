2cf9875d20dbbcfb8f27dbcb4c459a5f7ea7d454
sariyhpaulus <222112353@stis.ac.id>
2023-12-03 21:21:04 +0700

    menambahkan pertanyaan listing view

---
+++ collect_app/src/main/java/org/odk/collect/android/pkl/ui/theme/Typography.kt
@@ -0,0 +1,154 @@
+package org.odk.collect.android.pkl.ui.theme
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import org.odk.collect.android.R
+
+val PoppinsFontFamily = FontFamily(
+        Font(R.font.poppins),
+        Font(R.font.poppins_medium, FontWeight.Medium),
+        Font(R.font.poppins_light, FontWeight.Light),
+        Font(R.font.poppins_bold, FontWeight.Bold)
+)
+
+val poppins_reguler = Font(R.font.poppins)
+
+val Typography: Typography = Typography(
+    // Display Large - Poppins 57/64 . -0.25px
+    displayLarge = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 57.sp,
+        lineHeight = 64.sp,
+        letterSpacing = (-0.25).sp,
+    ),
+
+    // Display Medium - Poppins 45/52 . 0px
+    displayMedium = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 45.sp,
+        lineHeight = 52.sp,
+        letterSpacing = 0.sp,
+    ),
+
+    // Display Small - Poppins 36/44 . 0px
+    displaySmall = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 36.sp,
+        lineHeight = 44.sp,
+        letterSpacing = 0.sp,
+    ),
+
+    // Headline Large - Poppins 32/40 . 0px
+    headlineLarge = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+        letterSpacing = 0.sp,
+    ),
+
+    // Headline Medium - Poppins 28/36 . 0px
+    headlineMedium = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 28.sp,
+        lineHeight = 36.sp,
+        letterSpacing = 0.sp,
+    ),
+
+    // Headline Small - Poppins 24/32 . 0px
+    headlineSmall = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        letterSpacing = 0.sp,
+    ),
+
+    // Title Large - Poppins 22/28 . 0px
+    titleLarge = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp,
+    ),
+
+    // Title Medium - Poppins 16/24 . 0.15px
+    titleMedium = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.W500,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.15.sp,
+    ),
+
+    // Title Small - Poppins 14/20 . 0.1px
+    titleSmall = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp,
+    ),
+
+    // Label Large - Poppins 14/20 . 0.1px
+    labelLarge = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp,
+    ),
+
+    // Label Medium - Poppins 12/16 . 0.5px
+    labelMedium = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.W500,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp,
+    ),
+
+    // Label Small - Poppins 11/16 . 0.5px
+    labelSmall = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.W500,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp,
+    ),
+
+    // Body Large - Poppins 16/24 . 0.5px
+    bodyLarge = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp,
+    ),
+
+    // Body Medium - Poppins 14/20 . 0.25px
+    bodyMedium = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp,
+    ),
+    // Body Small - Poppins 12/16 . 0.4px
+    bodySmall = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.4.sp,
+    ),
+)
