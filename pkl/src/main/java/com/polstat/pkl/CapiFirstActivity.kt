ca985cec347cf62bf76372aebf6113bebd9ca984
Mjulianfr001056 <222112217@stis.ac.id>
2023-12-10 13:31:58 +0700

    Ngerapiin

--- pkl/src/main/java/com/polstat/pkl/CapiFirstActivity.kt
+++ pkl/src/main/java/com/polstat/pkl/CapiFirstActivity.kt
@@ -34,19 +34,3 @@ class CapiFirstActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Capi63Theme {
-        Greeting("Android")
-    }
-}
