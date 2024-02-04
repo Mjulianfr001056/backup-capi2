5ea07346871cc04434eb2024ac0098a4cbbbed99
222112038 <falanarofako29@gmail.com>
2024-02-04 21:40:15 +0700

    membuat tampilan upload foto kuesioner listing

--- pkl/src/main/java/com/polstat/pkl/utils/extension/Ext.kt
+++ pkl/src/main/java/com/polstat/pkl/utils/extension/Ext.kt
@@ -3,10 +3,17 @@ package com.polstat.pkl.utils.extension
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
+import android.util.Log
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.File
 import java.util.concurrent.Executor
 import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
 fun Context.hasLocationPermission(): Boolean {
@@ -19,7 +26,6 @@ fun Context.hasLocationPermission(): Boolean {
     ) == PackageManager.PERMISSION_GRANTED
 }
 
-@Suppress("BlockingMethodInNonBlockingContext")
 suspend fun Context.getCameraProvider(): ProcessCameraProvider = suspendCoroutine { continuation ->
     ProcessCameraProvider.getInstance(this).also { future ->
         future.addListener(
@@ -33,3 +39,4 @@ suspend fun Context.getCameraProvider(): ProcessCameraProvider = suspendCoroutin
 
 val Context.executor: Executor
     get() = ContextCompat.getMainExecutor(this)
+
